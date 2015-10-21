4f33799908c6836cdeedddf69afdefd707b37d16
Marcus Johansson <arcus.johansson@gmail.com>
2015-10-21 15:47:37 +0200

    instafeed js

--- src/js/main.js
+++ src/js/main.js
@@ -8,9 +8,23 @@ function showTip() {
 	});
 }
 
+function instaFeed() {
+	var clientId = 'd06ee162a3bb4abdbe112230442c0a77';
+	var feed = new Instafeed({
+		get: 'tagged',
+		tagName: 'filmfestival',
+		limit: 17,
+		sortBy: 'most-recent',
+		resolution: 'thumbnail',
+		clientId: clientId
+	});
+
+	feed.run();
+}
+
 
 $(document).ready(function() {
-
+	instaFeed();
 	showTip();
 	
 });
